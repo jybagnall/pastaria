3110b22c228841eb96d09c305d5eb7715178b518
Jiyoung <jybagnall@gmail.com>
2024-06-09 06:31:57 +0900

    add event logic

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 alert("Hi")
+
+const input = document.querySelector("#input");
+const result = document.querySelector("#result");
+const button = document.querySelector("#button");
+const list = document.querySelectorAll("li");
+
+input.addEventListener("input", function() {
+    const searchInput = input.value.toLowerCase().trim();
+    for (let fruit of list) {
+        const fruitText = fruit.innerText.toLowerCase();
+        if(fruitText.includes(searchInput)) {
+            fruit.style.display = "list-item"
+        } else {
+            fruit.style.display = "none"
+        }
+    }
+})
